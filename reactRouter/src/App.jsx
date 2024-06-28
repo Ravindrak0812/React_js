20d0559c6c04e67baad8618e6d9ac875dac9a8de
Ravindra Kumar <ravindra8122003@gmail.com>
2024-06-28 11:39:52 +0530

    Adding reactRouter Project

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Header/Footer/Footer'
+import Home from './components/Header/Home/Home'
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ reactRouter/src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Header/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <div>
+      <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+      </>
+    </div>
+  )
+}
